5995a392710ce2ee3649ec3929dd0ee3703d38cd
Max <max@recoset.com>
2015-06-03 00:47:35 -0400

    do not peep at cutting points

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::result::Result::{self, Ok, Err};
+use std::ops::Index;
 
 #[derive(Clone)]
 #[derive(Copy)]
@@ -9,6 +10,15 @@ enum Tile {
     Empty
 }
 
+type Board = [[Tile; 19]; 19];
+type Coord = (usize, usize);
+
+// impl Index<Coord> for Board {
+//     fn index(&self, index:Coord) -> Tile {
+//         self[index.0][index.1]
+//     }
+// }
+
 impl fmt::Display for Tile {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         formatter.write_str(
@@ -17,13 +27,38 @@ impl fmt::Display for Tile {
                 Tile::Black => "O",
                 Tile::Empty => ".",
             }
-        );
-        Ok(())
+        )
     }
 }
 
+// fn get_captures(board:&Board, coord:Coord ) -> Set<Coord> {
+    
+//     Set::new()
+// }
+
+// fn get_captures(board:&Board, coord:Coord, group:Set<Coord>, color:Tile) -> Set<Coord> {
+//     for neighbor in neighbors(group) {
+//         if !(group.contains(neighbor) || board[coord] == color) {
+
+//         }
+//     }
+// }
+
+fn neighbors(coord:Coord) -> Vec<Coord> {
+    let mut output:Vec<Coord> = Vec::new(); 
+    for offset in &[-1, 1] {
+        if (coord.0 + offset > 0) && (coord.0 + offset < 18) {
+            output.push((coord.0 + offset, coord.1));
+        }
+        if (coord.1 + offset > 0) && (coord.1 + offset < 18) {
+            output.push((coord.0, coord.1 + offset));
+        }
+    }
+    output
+}
+
 fn main(){
-    let board = &[[Tile::Empty; 19]; 19];
+    let board:&Board = &[[Tile::Empty; 19]; 19];
     // let board: Vec<Vec<Tile>> = vec![Vec::with_capacity(19), 19];
     for line in board {
         for tile in line {
